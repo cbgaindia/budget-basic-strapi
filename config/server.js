245c9727df6549011b388fbb030d2a05755d781e
Ubuntu <ubuntu@ip-172-31-41-84.eu-west-1.compute.internal>
2021-07-13 04:43:37 +0000

    updates

--- config/server.js
+++ config/server.js
@@ -1,7 +1,7 @@
 module.exports = ({ env }) => ({
   host: env('HOST', '0.0.0.0'),
   port: env.int('PORT', 8001),
-  url: 'http://52.30.188.232',
+  url: 'https://budgetbasics-backend.openbudgetsindia.org/',
   admin: {
     auth: {
       secret: env('ADMIN_JWT_SECRET', 'e4fd0654323afc22ba38138a0f7e0e91'),
