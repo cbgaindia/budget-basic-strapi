f5f99e004678b33289d3f454fb83d3b0f8901f73
PixeledCode <ashoaib540918@gmail.com>
2021-07-15 11:14:19 +0530

    config files update

--- config/server.js
+++ config/server.js
@@ -1,11 +1,11 @@
 module.exports = ({ env }) => ({
-  host: env('HOST', '0.0.0.0'),
-  port: env.int('PORT', 8001),
-  url: 'https://budgetbasics.openbudgetsindia.org/api',
+  host: env('HOST'),
+  port: env.int('PORT'),
+  url: env('STRAPI_API'),
   admin: {
-    url: 'https://budgetbasics.openbudgetsindia.org/dashboard',	  
+    url: env('STRAPI_DASHBOARD'),
     auth: {
-      secret: env('ADMIN_JWT_SECRET', 'e4fd0654323afc22ba38138a0f7e0e91'),
+      secret: env('STRAPI_JWT_SECRET'),
     },
   },
 });
