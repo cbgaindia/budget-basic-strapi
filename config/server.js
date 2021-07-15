75286441a850e85e0e39fbbfbc3e648e9e8370df
PixeledCode <ashoaib540918@gmail.com>
2021-07-15 12:15:57 +0530

    fixed server.js file

--- config/server.js
+++ config/server.js
@@ -1,9 +1,9 @@
 module.exports = ({ env }) => ({
   host: env('HOST'),
   port: env.int('PORT'),
-  // url: env('STRAPI_API'),
+  url: env('STRAPI_API'),
   admin: {
-    // url: env('STRAPI_DASHBOARD'),
+    url: env('STRAPI_DASHBOARD'),
     auth: {
       secret: env('STRAPI_JWT_SECRET'),
     },
