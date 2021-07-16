60ed067ce4efd65008767f96bcd0cfa8b8862f94
PixeledCode <ashoaib540918@gmail.com>
2021-07-16 17:27:30 +0530

    suggested article fixed

--- api/suggested-articles/controllers/suggested-articles.js
+++ api/suggested-articles/controllers/suggested-articles.js
@@ -8,7 +8,7 @@ module.exports = {
    */
 
   async find(ctx) {
-    const entity = await strapi.services['suggested-articles'].find(['articles.chapter']);
+    const entity = await strapi.services['suggested-articles'].find({}, ['articles.chapter']);
     return sanitizeEntity(entity, { model: strapi.models['suggested-articles'] });
   },
 };
