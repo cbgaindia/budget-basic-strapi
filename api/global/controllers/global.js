937d0035c5ba8f021a8146e77acf09f75c1de6f2
PixeledCode <ashoaib540918@gmail.com>
2021-07-27 18:17:00 +0530

    global changes

--- api/global/controllers/global.js
+++ api/global/controllers/global.js
@@ -1,8 +1,14 @@
-'use strict';
+const { sanitizeEntity } = require('strapi-utils');
 
-/**
- * Read the documentation (https://strapi.io/documentation/developer-docs/latest/development/backend-customization.html#core-controllers)
- * to customize this controller
- */
+module.exports = {
+  /**
+   * Retrieve the record.
+   *
+   * @return {Object}
+   */
 
-module.exports = {};
+  async find(ctx) {
+    const entity = await strapi.services.global.find({}, ['articles.chapter']);
+    return sanitizeEntity(entity, { model: strapi.models.global });
+  },
+};
