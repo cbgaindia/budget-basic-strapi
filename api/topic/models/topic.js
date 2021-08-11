b2693c3f9783a4eda6b9f228c898c0139e29a959
PixeledCode <ashoaib540918@gmail.com>
2021-08-11 20:18:19 +0530

    topic

--- api/topic/models/topic.js
+++ api/topic/models/topic.js
@@ -1,8 +1,8 @@
-'use strict';
-
-/**
- * Read the documentation (https://strapi.io/documentation/developer-docs/latest/development/backend-customization.html#lifecycle-hooks)
- * to customize this model
- */
-
-module.exports = {};
+module.exports = {
+  lifecycles: {
+    beforeCreate(data) {
+      console.log(data);
+      data.Title = 'Some fixed name';
+    },
+  },
+};
