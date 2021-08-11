abb980374a7820817f64573b8bf1c905ed800e05
Shoaib Ahmed <ashoaib540918@gmail.com>
2021-08-11 23:32:12 +0530

    added new formatted content

--- api/topic/models/topic.js
+++ api/topic/models/topic.js
@@ -1,8 +1,23 @@
+const jsdom = require("jsdom");
+const { JSDOM } = jsdom;
+
+function extractContent(s) {
+  // add spacing before tag ends
+  const search = "</";
+  const replacer = new RegExp(search, "g");
+  formattedContent = s.replace(replacer, " </");
+
+  const dom = new JSDOM(`<!DOCTYPE html><span>${formattedContent}</span>`);
+  return (
+    dom.window.document.querySelector("span").textContent ||
+    dom.window.document.querySelector("span").innerText
+  );
+}
+
 module.exports = {
   lifecycles: {
     async beforeCreate(data) {
-      console.log(data);
-      data.Title = 'Some fixed name';    
+      data.formattedContent = extractContent(data.Content);
     },
   },
 };
