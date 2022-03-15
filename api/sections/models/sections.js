791d4f360ca18d4c838580d49f073ef79dc63e75
Shoaib Ahmed <ashoaib540918@gmail.com>
2022-03-15 15:39:07 +0530

    formattedcontenthindi

--- api/sections/models/sections.js
+++ api/sections/models/sections.js
@@ -2,13 +2,10 @@ const jsdom = require("jsdom");
 const { JSDOM } = jsdom;
 
 function extractContent(s) {
-  console.log(s);
   // add spacing before tag ends
   const search = "</";
   const replacer = new RegExp(search, "g");
   const formattedContent = s.replace(replacer, " </");
-
-  // .replace(/\s\s+/g, ' ') - incase of multiple whitespaces, change them to single whitespace
 
   const dom = new JSDOM(`<!DOCTYPE html><span>${formattedContent}</span>`);
   const span = dom.window.document.querySelector("span")
@@ -22,10 +19,12 @@ module.exports = {
   lifecycles: {
     async beforeCreate(data) {
       data.formattedContent = extractContent(data.Content);
+      data.formattedContentHindi = extractContent(data.Content);
     },
     async beforeUpdate(params, data) {
       if(data.Content)
         data.formattedContent = extractContent(data.Content);
+        data.formattedContentHindi = extractContent(data.Content);
     },
   },
 };
